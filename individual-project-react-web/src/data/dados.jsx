477a582b72740ebb386910d1bdf4d2233fba3c89
pedromartins01 <pedropintomartinsdesouza@gmail.com>
2026-06-04 14:59:56 -0300

    Criando dados do projeto

---
+++ individual-project-react-web/src/data/dados.jsx
@@ -0,0 +1,91 @@
+export const produtos = [ 
+    {
+        id: 1,
+        nome: 'Beterraba',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 7.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 2,
+        nome: 'Batata',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 7.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 3,
+        nome: 'Cenoura',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 3.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 4,
+        nome: 'Couve',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 1.99,
+        unidade: 'folha'
+
+    },
+
+    {
+        id: 5,
+        nome: 'Tomate',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 8.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 6,
+        nome: 'Alho',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 14.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 7,
+        nome: 'Cebola',
+        categoria: 'Legumes, verduras e vegetais',
+        preco: 4.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 8,
+        nome: 'Banana',
+        categoria: 'Frutas',
+        preco: 5.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 9,
+        nome: 'Maçã',
+        categoria: 'Frutas',
+        preco: 6.99,
+        unidade: 'kg'
+
+    },
+
+    {
+        id: 10,
+        nome: 'Laranja',
+        categoria: 'Frutas',
+        preco: 7.99,
+        unidade: 'kg'
+
+    }
+]

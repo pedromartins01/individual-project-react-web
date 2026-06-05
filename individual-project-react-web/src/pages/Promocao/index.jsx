2e3ac96f91404837ebc0b5cc908d6d1774ae4f1c
pedromartins01 <pedropintomartinsdesouza@gmail.com>
2026-06-05 12:57:15 -0300

    Alterações e correções

--- individual-project-react-web/src/pages/Promocao/index.jsx
+++ individual-project-react-web/src/pages/Promocao/index.jsx
@@ -10,7 +10,7 @@ export const Promocao = ({ voltarParaHome }) => {
             <CardPromocaoStyle>
                 <CardPromocaoTitulo>OFERTA DA SEMANA</CardPromocaoTitulo>
                 <CardPromocaoTexto>
-                    Na próxima semana, todas as frutas estarão com uma promoção de  <strong>LEVE 1kg</strong> e pague  <strong>APENAS 700gr</strong>!
+                    Nesta semana, todas as frutas estarão com uma promoção de  <strong>LEVE 1kg</strong> e pague  <strong>APENAS 700gr</strong>!
                 </CardPromocaoTexto>
                 <CardPromocaoTexto>
                     Não perca! Cuide da sua saúde e ao mesmo tempo do seu bolso!
